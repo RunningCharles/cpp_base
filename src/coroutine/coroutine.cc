ae10f5aabb575324dadc03b86e52b58caf859962
leogcchen <leogcchen@tencent.com>
2024-05-17 14:56:33 +0800

    创建协程文件

---
+++ src/coroutine/coroutine.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "coroutine.h"
+
+void Fun() {
+  std::cout << 1 << std::endl;
+  std::cout << 2 << std::endl;
+  std::cout << 3 << std::endl;
+  std::cout << 4 << std::endl;
+}
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
 #include <iostream>
+#include "./coroutine/coroutine.h"
 
 int main(int argc, char *argv[]){
+    Fun();
     std::cout << "Hello, from cpp_base!\n";
 }
