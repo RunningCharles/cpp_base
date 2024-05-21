6c281db06a630a389162398023c3ed3f84dd9256
leogcchen <leogcchen@tencent.com>
2024-05-21 16:45:49 +0800

    生成器代码完成

--- src/coroutine/original_co.cc
+++ src/coroutine/original_co.cc
@@ -6,33 +6,50 @@ namespace co {
 namespace original {
 
 struct Generator {
-  struct promise_type {
-    uint32_t value;
 
+  // 协程执行完成之后，外部读取值时抛出的异常
+  class ExhausteException: std::exception {};
+
+  struct promise_type {
+    int32_t value = -1;
+    bool is_ready = false;
+
+    // 开始执行时直接挂起等待外部调用 resume 获取下一个值
     std::suspend_always initial_suspend() {
       std::cout << "generator initial suspend" << std::endl;
       return {};
     }
     
-    std::suspend_never final_suspend() noexcept {
+    // 执行结束后不需要挂起
+    // std::suspend_never final_suspend() noexcept {
+    //   std::cout << "generator final suspend" << std::endl;
+    //   return {};
+    // }
+
+    // 总是挂起，让 Generator 来销毁
+    std::suspend_always final_suspend() noexcept {
       std::cout << "generator final suspend" << std::endl;
       return {};
     }
 
+    // 不会抛出异常，这里不做任何处理
     void unhandled_exception() {
       std::cout << "generator unhandled exception" << std::endl;
     }
-
+    
+    // 构造协程的返回值类型，绑定 handle
     Generator get_return_object() {
       std::cout << "generator get return object" << std::endl;
       return Generator{ std::coroutine_handle<promise_type>::from_promise(*this) };
     }
 
+    // 没有返回值
     void return_void() {
       std::cout << "generator return void" << std::endl;
     }
-
-    std::suspend_always await_transform(uint32_t value) {
+    
+    // 将基本类型转化为 awaiter
+    std::suspend_always await_transform(int32_t value) {
       std::cout << "generator await transform: " << this->value << " to " << value << std::endl;
       this->value = value;
       return {};
@@ -41,22 +58,73 @@ struct Generator {
 
   std::coroutine_handle<promise_type> handle;
 
-  uint32_t next() {
-    handle.resume();
-    return handle.promise().value;
+  // 显示构造函数，禁止隐式转换
+  explicit Generator(std::coroutine_handle<promise_type> handle) noexcept
+    : handle(handle) {}
+
+  
+  // 对于每一个协程实例，都有且仅能有一个 Generator 实例与之对应，因此只支持移动对象，而不支持复制对象。
+  Generator(Generator &&generator) noexcept
+    : handle(std::exchange(generator.handle, {})) {}
+  Generator(Generator &) = delete;
+  Generator &operator=(Generator &) = delete;
+
+  ~Generator() {
+    std::cout << "generator destroy" << std::endl;
+    // 销毁协程
+    handle.destroy();
+  }
+
+  bool has_next() {
+    // 协程已经执行完成
+    if (handle.done()) {
+      std::cout << "generator has next, done(1)" << std::endl;
+      return false;
+    }
+
+    // 协程还没有执行完成，并且下一个值还没有准备好
+    if (!handle.promise().is_ready) {
+      std::cout << "generator has next, hasn't done, not ready" << std::endl;
+      handle.resume();
+      handle.promise().is_ready = true;
+    }
+    
+    if (handle.done()) {
+      // 恢复执行之后协程执行完，这时候必然没有通过 co_await 传出值来
+      std::cout << "generator has next, done(2)" << std::endl;
+      return false;
+    } else {
+      std::cout << "generator has next, hasn't done" << std::endl;
+      return true;
+    }
+  }
+
+  int32_t next() {
+    if (has_next()) {
+      // 此时一定有值，is_ready 为 true 
+      // 消费当前的值，重置 is_ready 为 false
+      handle.promise().is_ready = false;
+      return handle.promise().value;
+    }
+
+    throw ExhausteException();
   }
 };
 
 Generator sequence() {
-  for (uint32_t i = 0; i < 10; i++) {
+  for (int32_t i = 0; i < 10; i++) {
     co_await i;
   }
 }
 
 void RunGenerator() {
   auto gen = sequence();
-  for (uint32_t i = 0; i < 15; i++) {
-    std::cout << gen.next() << std::endl;
+  for (int32_t i = 0; i < 15; i++) {
+    if (gen.has_next()) {
+      std::cout << gen.next() << std::endl;
+    } else {
+      break;
+    }
   }
 }
 
